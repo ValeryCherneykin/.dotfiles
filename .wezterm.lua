a0833d6d72cec5cdbe17cb9af7d840c6591047df
Valery Cherneykin <chernecin@icloud.com>
2025-05-25 16:41:17 +0300

    Fast config wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,38 +1,44 @@
 local wezterm = require("wezterm")
 
 return {
-  color_scheme = "Tokyo Night",
+    color_scheme = "Tokyo Night",
 
-  font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Regular" }),
-  font_size = 14,
+    font = wezterm.font("JetBrains Mono", { weight = "Regular" }),
+    font_size = 14,
+    freetype_load_target = "Light",
+    freetype_render_target = "HorizontalLcd",
 
-  window_background_image = 'path/to/image',
-  window_background_image_hsb = {
-    hue = 1.0,
-    saturation = 1.0,
-    brightness = 1.0,
-  },
-  window_background_opacity = 0.3,
-  text_background_opacity = 0.7,
+    front_end = "WebGpu",
+    max_fps = 60,
+    animation_fps = 1,
 
-  keys = {
-    { key = "h", mods = "CMD", action = wezterm.action.ActivatePaneDirection("Left") },
-    { key = "l", mods = "CMD", action = wezterm.action.ActivatePaneDirection("Right") },
-    { key = "k", mods = "CMD", action = wezterm.action.ActivatePaneDirection("Up") },
-    { key = "j", mods = "CMD", action = wezterm.action.ActivatePaneDirection("Down") },
-    { key = "t", mods = "CMD", action = wezterm.action.SpawnTab("CurrentPaneDomain") },
-    { key = "w", mods = "CMD", action = wezterm.action.CloseCurrentTab({ confirm = true }) },
-  },
+    -- window_background_image = "/path/",
+    -- window_background_image_hsb = {
+    --     hue = 1.0,
+    --     saturation = 0.5,
+    --     brightness = 0.1,
+    -- },
 
-  window_decorations = "RESIZE",
-  native_macos_fullscreen_mode = true,
+    window_background_opacity = 1.0,
+    text_background_opacity = 1.0,
 
-  enable_tab_bar = false,
-  use_fancy_tab_bar = false,
-  hide_tab_bar_if_only_one_tab = true,
+    enable_tab_bar = false,
+    enable_scroll_bar = false,
+    window_decorations = "RESIZE",
+    native_macos_fullscreen_mode = true,
 
-  wezterm.on("gui-startup", function(cmd)
-    local tab, pane, window = wezterm.mux.spawn_window(cmd or {})
-    pane:send_text("tmux new-session -As main 'neofetch; exec zsh'\n")
-  end),
+    check_for_updates = false,
+    use_dead_keys = false,
+    enable_kitty_keyboard = false,
+
+    default_prog = { "/bin/zsh", "-l" },
+    keys = {
+        {
+            key = 'f',
+            mods = 'CTRL',
+            action = wezterm.action.SpawnCommandInNewTab {
+                args = { "/bin/zsh", "-i", "-l", "~/bin/dev.sh" },
+            },
+        },
+    }
 }
