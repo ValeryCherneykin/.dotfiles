b897e89287c0fa8aab2c20b24f9acc0472ed37d1
Valery Cherneykin <chernecin@icloud.com>
2025-07-16 18:08:55 +0300

    Added desing rose-pine-moon

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require("wezterm")
 
 return {
-    color_scheme = "Catppuccin Mocha",
+    color_scheme = "rose-pine-moon",
 
     font = wezterm.font_with_fallback({
         { family = "FiraCode Nerd Font", weight = "Regular" },
